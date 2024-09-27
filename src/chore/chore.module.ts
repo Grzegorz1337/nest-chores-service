b7b0f31b1ac7b8d170337dd83f1df130dbd7ee46
Kamil Grzeczkowski <kamilgrzeczkowski@gmail.com>
2024-09-27 18:09:50 +0200

    Fix connection to mongo

--- src/chore/chore.module.ts
+++ src/chore/chore.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ChoreController } from './controller/chore.controller';
 import { ChoreService } from './service/chore.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [ChoreController],
   providers: [ChoreService],
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          uri: config.get<string>('MONGODB_URI'),
+          user: config.get<string>('DATABASE_USER'),
+          pass: config.get<string>('DATABASE_PASS'),
+          dbName: config.get<string>('DATABASE_NAME'),
+        };
+      },
+    }),
+  ],
 })
 export class ChoreModule {}
