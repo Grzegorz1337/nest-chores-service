5749e0965fe02bd0d4580f88d4044f4f0fe5b30d
Kamil Grzeczkowski <kamilgrzeczkowski@gmail.com>
2024-09-30 19:12:33 +0200

    Finish e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,41 +2,104 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import * as typeorm from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ChorePlace } from 'src/chore/model/chore-place.enum';
+import { Chore } from 'src/chore/model/chore.schema';
+import { randomUUID } from 'crypto';
 
 describe('ChoresController (e2e)', () => {
   let app: INestApplication;
+  let choreRepository: typeorm.Repository<Chore>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    choreRepository = moduleFixture.get<typeorm.Repository<Chore>>(
+      getRepositoryToken(Chore),
+    );
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
   });
 
-  it('GET /chores', async () => {
-    await request(app.getHttpServer()).get('/chores').expect(200).expect([]);
+  describe('GET /chores', async () => {
+    it('Returns chore list when there are chores present', async () => {
+      const chore = {
+        id: randomUUID(),
+        creationDate: Date.now(),
+        expirationDate: Date.now(),
+        chorePlace: ChorePlace.KITCHEN,
+        choreDescription: 'Clean counter',
+        orderedBy: { name: 'Iga', surname: 'Lukiewska' },
+        orderedFor: { name: 'Kamil', surname: 'Grzeczkowski' },
+        completed: false,
+      };
+      await choreRepository.save(chore);
+      await request(app.getHttpServer())
+        .get('/chores')
+        .expect(200)
+        .expect([chore]);
+    });
+
+    it('Returns 404 when no chore is in database', async () => {
+      await request(app.getHttpServer()).get('/chores').expect(404);
+    });
   });
 
-  // TODO: add population of database with some example data
-  it('GET /chores/choreId', async () => {
-    await request(app.getHttpServer())
-      .get('/chores/1-1-1-1')
-      .expect(200)
-      .expect([]);
+  describe('GET /chores/choreId', async () => {
+    it('Returns a chore when id is valid', async () => {
+      const chore = {
+        id: randomUUID(),
+        creationDate: Date.now(),
+        expirationDate: Date.now(),
+        chorePlace: ChorePlace.KITCHEN,
+        choreDescription: 'Clean counter',
+        orderedBy: { name: 'Iga', surname: 'Lukiewska' },
+        orderedFor: { name: 'Kamil', surname: 'Grzeczkowski' },
+        completed: false,
+      };
+      await choreRepository.save(chore);
+      await request(app.getHttpServer())
+        .get(`/chores/${chore.id}`)
+        .expect(200)
+        .expect(chore);
+    });
+
+    it('Returns 404 when no chore was found', async () => {
+      await request(app.getHttpServer()).get('/chores/1-2-3-4').expect(404);
+    });
   });
 
-  it('GET /chores/active', async () => {
-    await request(app.getHttpServer())
-      .get('/chores/active')
-      .expect(200)
-      .expect([]);
+  describe('GET /chores/active', async () => {
+    it('Returns list of active chores if any is present', async () => {
+      const chore = {
+        id: randomUUID(),
+        creationDate: Date.now(),
+        expirationDate: Date.now(),
+        chorePlace: ChorePlace.KITCHEN,
+        choreDescription: 'Clean counter',
+        orderedBy: { name: 'Iga', surname: 'Lukiewska' },
+        orderedFor: { name: 'Kamil', surname: 'Grzeczkowski' },
+        completed: false,
+      };
+      await choreRepository.save(chore);
+      await request(app.getHttpServer())
+        .get('/chores/active')
+        .expect(200)
+        .expect([chore]);
+    });
+
+    it('Returns 4-4 when there are no active chores todo', async () => {
+      await request(app.getHttpServer()).get('/chores/active').expect(404);
+    });
   });
 
   it('POST /chores', async () => {
     const newChore = {
-      chorePlace: 'Backyard',
+      chorePlace: ChorePlace.YARD,
       choreDescription: 'Bring some logs for fireplace',
       orderedBy: {
         name: 'Iga',
@@ -62,22 +125,51 @@ describe('ChoresController (e2e)', () => {
         expect(body.creationDate).toBeDefined();
         expect(body.expirationDate).toBeDefined();
       });
+    expect(
+      await choreRepository.existsBy({
+        choreDescription: 'Bring some logs for fireplace',
+      }),
+    ).toBeTruthy();
   });
 
-  // TODO: Add data population
   it('PATCH /chores/choreId', async () => {
-    await request(app.getHttpServer())
-      .patch('/chores/1-1-1-1')
-      .expect(200)
-      .expect([]);
+    const chore = {
+      id: randomUUID(),
+      creationDate: Date.now(),
+      expirationDate: Date.now(),
+      chorePlace: ChorePlace.KITCHEN,
+      choreDescription: 'Clean counter',
+      orderedBy: { name: 'Iga', surname: 'Lukiewska' },
+      orderedFor: { name: 'Kamil', surname: 'Grzeczkowski' },
+      completed: false,
+    };
+    await choreRepository.save(chore);
+    await request(app.getHttpServer()).patch(`/chores/${chore.id}`).expect(200);
+    const updatedChore = await choreRepository.findOneBy({ id: chore.id });
+    expect(updatedChore.completed).toBe(true);
   });
 
-  // TODO: Add data population
   it('DELETE /chores/choreId', async () => {
+    const chore = {
+      id: randomUUID(),
+      creationDate: Date.now(),
+      expirationDate: Date.now(),
+      chorePlace: ChorePlace.KITCHEN,
+      choreDescription: 'Clean counter',
+      orderedBy: { name: 'Iga', surname: 'Lukiewska' },
+      orderedFor: { name: 'Kamil', surname: 'Grzeczkowski' },
+      completed: false,
+    };
+    await choreRepository.save(chore);
     await request(app.getHttpServer())
-      .delete('/chores/1-1-1-1')
+      .delete(`/chores/${chore.id}`)
       .expect(200)
       .expect([]);
+    expect(await choreRepository.existsBy({ id: chore.id })).toBeFalsy();
+  });
+
+  afterEach(async () => {
+    await choreRepository.clear();
   });
 
   afterAll(async () => {
